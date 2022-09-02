59310afcbc62ecd98b410fd98b60184d42d1d742
Lucas Cabral <lucas.9cabral@gmail.com>
2022-09-02 16:39:39 -0300

    Feature:Implementa formatação do nome de Cartão, do numero de cartão e geração da data de validade.

--- src/controllers/companiesController.ts
+++ src/controllers/companiesController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { buscarEmpresa, buscarUsuario } from "../services/companiesService";
+import { buscarEmpresa, buscarUsuario, formaNumeroCartaoFormatado, formatarNomeCartao, geraDataDeValidade, geraCodigoCvc } from "../services/companiesService";
 
 export async function createCards(req: Request, res: Response) {
     const apiKey = res.locals.apiKey
@@ -7,9 +7,15 @@ export async function createCards(req: Request, res: Response) {
     const idUsuario = Number(req.params.idUser)
     try {
         const verificaEmpresa = await buscarEmpresa(apiKey)
-        const varificaIdUsuario = await buscarUsuario(idUsuario)
+        const verificaIdUsuario = await buscarUsuario(idUsuario)
+        const numeroCartao = await formaNumeroCartaoFormatado()
+        const nomeCartao = await formatarNomeCartao(verificaIdUsuario.fullName)
+        const dataVencimento = await geraDataDeValidade()
+        const codigoCvc = await geraCodigoCvc()
 
-        res.status(200).send(varificaIdUsuario)
+        console.log(dataVencimento)
+
+        res.status(200).send(verificaIdUsuario)
     } catch ({ code, message }) {
         if (code === "NotFound") {
             return res.status(404).send(message)
--- src/services/companiesService.ts
+++ src/services/companiesService.ts
@@ -1,4 +1,6 @@
 import * as companiesRepository from "../repositories/companiesRepository"
+import { faker } from '@faker-js/faker';
+import dayjs from "dayjs"
 
 export async function buscarEmpresa(apiKey: string) {
 
@@ -18,5 +20,42 @@ export async function buscarUsuario(idUsuario: number) {
         throw { code: "NotFound", message: "Esse usuário não existe" }
     }
 
-    return existeUsuario
+    return existeUsuario[0]
+}
+
+export async function formatarNomeCartao(nome: string) {
+    const nomeArray = nome.split(" ")
+    const primeiroNome = nomeArray[0]
+    const ultimoNome = nomeArray[nomeArray.length - 1]
+
+    const formarNome = []
+
+    for (let i = 1; i < nomeArray.length - 1; i++) {
+        if (nomeArray[i].length > 3) {
+            formarNome.push(nomeArray[i][0])
+        }
+    }
+    formarNome.unshift(primeiroNome)
+    formarNome.push(ultimoNome)
+
+    return formarNome.join(" ").toUpperCase()
+}
+
+export async function formaNumeroCartaoFormatado() {
+    const numeroCartaoRandom = faker.random.numeric(16);
+
+    let numeroCartaoFormatado = numeroCartaoRandom.slice(0, 4) + " " + numeroCartaoRandom.slice(4, 8) + " " + numeroCartaoRandom.slice(8, 12) + " " + numeroCartaoRandom.slice(12, 16)
+    return numeroCartaoFormatado
+}
+
+export async function geraDataDeValidade() {
+    const dataAtual = dayjs().format("DD/MM/YYYY")
+    const dataArray = dataAtual.split("/")
+    const anoVencimento = Number(dataArray[2]) + 5
+    const dataVencimento = `${dataArray[1]}/${anoVencimento.toString()}`
+
+    return dataVencimento
+}
+export async function geraCodigoCvc() {
+    const codigoCvc = faker.random.numeric(3)
 }
